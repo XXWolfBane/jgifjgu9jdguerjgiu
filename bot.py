df7fe6a86539fdb6fdf9cc34ef4a8c42e32b0ca1
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-27 01:32:50 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ async def on_ready(ctx):
     print('---------')
     
 @bot.command()
-async def updates(ctx)
+async def updates(ctx):
     embed=discord.Embed(title="Updates", description="This shows all recent updates", color=0xedfb40)
     embed.add_field(name=Update 0.01, value=+ Bot starting, inline=True)
     embed.set_footer(text="This bot was made by WolfBane#5559")
