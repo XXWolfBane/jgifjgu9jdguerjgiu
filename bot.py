6e581e00405ebefdba6a80dce370859278cfb814
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-27 01:42:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 import discord
 from discord.ext import commands
 
-@client.event()
+bot = commands.Bot(command_prefix='VB!')
+
+@bot.event()
 async def on_ready(ctx):
     print('Logged in as')
-    print(client.user)
-    print(client.userid)
+    print(bot.user)
+    print(bot.userid)
     print('---------')
     
-@client.command()
+@bot.command()
 async def updates(ctx):
     embed=discord.Embed(title="Updates", description="This shows all recent updates", color=0xedfb40)
     embed.add_field(name="Update 0.01", value="Bot starting", inline=True)
